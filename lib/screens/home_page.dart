07396bc9f3e06b4991f93a8ed133f1cae5feca44
William Alvis <alvis.wap@gmail.com>
2021-01-22 18:48:52 -0500

    implement example given to json

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,12 +1,57 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('Hola munfo'),),
+      body: FutureBuilder(
+        future: fetchPhotos(http.Client()),
+        builder: (BuildContext context, AsyncSnapshot<List<Photo>> snapshot) {
+          if (snapshot.data is List<Photo>) {
+            return ListView.builder(
+                itemCount: snapshot.data.length,
+                itemBuilder: (BuildContext ctxt, int Index) {
+                  return Text(snapshot.data[Index].title);
+                });
+          } else {
+            return Center(
+              child: Text('Error no hay informacion'),
+            );
+          }
+        },
+      ),
     );
   }
 }
 
+List<Photo> parsePhotos(String responseBody) {
+  final parsed = jsonDecode(responseBody).cast<Map<String, dynamic>>();
 
+  return parsed.map<Photo>((json) => Photo.fromJson(json)).toList();
+}
+
+Future<List<Photo>> fetchPhotos(http.Client client) async {
+  final response = await client.get('https://jsonplaceholder.typicode.com/photos');
+
+  return compute(parsePhotos, response.body);
+}
+
+class Photo {
+  final int id;
+  final String title;
+  final String thumbnailUrl;
+
+  Photo({this.id, this.title, this.thumbnailUrl});
+
+  factory Photo.fromJson(Map<String, dynamic> json) {
+    return Photo(
+      id: json['id'] as int,
+      title: json['title'] as String,
+      thumbnailUrl: json['thumbnailUrl'] as String,
+    );
+  }
+}
