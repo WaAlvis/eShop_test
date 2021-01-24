607462b638926f2e36636edba51e37d5fd10d2ae
William Alvis <alvis.wap@gmail.com>
2021-01-24 15:43:51 -0500

    implement DetailScreen with show data product from view List & Grid

--- lib/utilies/custom_widgets/list_view_widget_custom.dart
+++ lib/utilies/custom_widgets/list_view_widget_custom.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
 
+import 'package:e_shop_test/screens/detail_product_screen.dart';
 import 'package:e_shop_test/services/get_networking.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
@@ -17,53 +18,67 @@ class ListViewWidgetCustom extends StatelessWidget {
     return ListView.builder(
       itemCount: dataUrl.data.length,
       itemBuilder: (BuildContext ctxt, int Index) {
-        return Card(
-          child: Row(
-            children: [
-              Expanded(
-                child: Image.network(
-                  dataUrl.data[Index].image,
-                  fit: BoxFit.cover,
+        return GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) {
+                  return DetailProductScreen(
+                    dataProduct: dataUrl.data[Index],
+                  );
+                },
+              ),
+            );
+          },
+          child: Card(
+            child: Row(
+              children: [
+                Expanded(
+                  child: Image.network(
+                    dataUrl.data[Index].image,
+                    fit: BoxFit.cover,
+                  ),
                 ),
-              ),
-              SizedBox(
-                width: 14,
-              ),
-              Expanded(
-                flex: 2,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Text(
-                      dataUrl.data[Index].productName,
-                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
-                      maxLines: 2,
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Text('Free Shoping'),
-                    Align(
-                      child: Text(
-                        '\$ 2.323',
-                        style: TextStyle(fontWeight: FontWeight.w600),
-                      ),
-                      alignment: Alignment.centerLeft,
-                    ),
-                    RatingBarIndicator(
-                      rating: rateRandom(),
-                      itemBuilder: (context, index) => Icon(
-                        Icons.star,
-                        color: Colors.amber,
-                      ),
-                      itemCount: 5,
-                      itemSize: 15.0,
-                    ),
-                  ],
+                SizedBox(
+                  width: 14,
                 ),
-              ),
-            ],
+                Expanded(
+                  flex: 2,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Text(
+                        dataUrl.data[Index].productName,
+                        style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
+                        maxLines: 2,
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text('Free Shoping'),
+                      Align(
+                        child: Text(
+                          '\$ 2.323',
+                          style: TextStyle(fontWeight: FontWeight.w600),
+                        ),
+                        alignment: Alignment.centerLeft,
+                      ),
+                      RatingBarIndicator(
+                        rating: rateRandom(),
+                        itemBuilder: (context, index) => Icon(
+                          Icons.star,
+                          color: Colors.amber,
+                        ),
+                        itemCount: 5,
+                        itemSize: 15.0,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         );
       },
