0cdcd26e8b1f7fdb66e13c15188609e3f7774669
William Alvis <alvis.wap@gmail.com>
2021-01-24 10:33:58 -0500

    show data in Grid

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -12,15 +12,53 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        leading: GestureDetector(
+            // child: viewList == false ? Icon(Icons.list) : Icon(Icons.grid_view),
+            // onTap: () {
+            //   viewList = !viewList;
+            //   changeMode();
+            // }
+        ),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text('e-Shop'),
+            Spacer(),
+            Icon(Icons.shopping_cart_rounded),
+          ],
+        ),
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(40.0),
+          child: Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 20.0, top: 30, bottom: 20),
+                child: Text(
+                  'Lista de productos',
+                  style: Theme.of(context).textTheme.headline6.copyWith(color: Colors.white),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
       body: FutureBuilder(
         future: getProductsData(),
         builder: (BuildContext context, AsyncSnapshot<List<Product>> snapshot) {
           if (snapshot.connectionState == ConnectionState.done) {
-            return ListView.builder(
+            return
+              GridView.builder(
                 itemCount: snapshot.data.length,
-                itemBuilder: (BuildContext ctxt, int Index) {
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 4.0,
+                    mainAxisSpacing: 4.0
+                ),
+                itemBuilder: (BuildContext context, int Index){
                   return Image.network(snapshot.data[Index].image);
-                });
+                },
+              );
           } else {
             return Center(
               child: CircularProgressIndicator(),
