5f6a54755bfff5f52d2fdf833e1980d2b15ab74d
William Alvis <alvis.wap@gmail.com>
2021-01-24 14:29:04 -0500

    view list products ok, debug icon change viewProducts

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,7 +1,10 @@
+import 'dart:math';
+
 import 'package:e_shop_test/utilies/custom_widgets/GridViewCustom.dart';
 import 'package:e_shop_test/utilies/custom_widgets/app_bar_custom.dart';
 import 'package:flutter/material.dart';
 import 'package:e_shop_test/services/get_networking.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
@@ -41,7 +44,60 @@ class _HomePageState extends State<HomePage> {
                 ? GridViewCustom(
                     dataUrl: snapshot,
                   )
-                : Center(child: Text('data'));
+                : ListView.builder(
+                    itemCount: snapshot.data.length,
+                    itemBuilder: (BuildContext ctxt, int Index) {
+                      return Card(
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: Image.network(
+                                snapshot.data[Index].image,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            SizedBox(
+                              width: 14,
+                            ),
+                            Expanded(
+                              flex: 2,
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                crossAxisAlignment: CrossAxisAlignment.stretch,
+                                children: [
+                                  Text(
+                                    snapshot.data[Index].productName,
+                                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
+                                    maxLines: 2,
+                                  ),
+                                  SizedBox(
+                                    height: 20,
+                                  ),
+                                  Text('Free Shoping'),
+                                  Align(
+                                    child: Text(
+                                      '\$ 2.323',
+                                      style: TextStyle(fontWeight: FontWeight.w600),
+                                    ),
+                                    alignment: Alignment.centerLeft,
+                                  ),
+                                  RatingBarIndicator(
+                                    rating: rateRandom(),
+                                    itemBuilder: (context, index) => Icon(
+                                      Icons.star,
+                                      color: Colors.amber,
+                                    ),
+                                    itemCount: 5,
+                                    itemSize: 15.0,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  );
           } else {
             return Center(
               child: CircularProgressIndicator(),
@@ -51,4 +107,9 @@ class _HomePageState extends State<HomePage> {
       ),
     );
   }
+}
+
+rateRandom() {
+  Random rnd = new Random();
+  return 1 + rnd.nextDouble() * 4;
 }
