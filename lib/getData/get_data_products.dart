779af489b31aac0c34db843d9e3625e0621aa608
William Alvis <alvis.wap@gmail.com>
2021-01-23 19:22:44 -0500

    implementation POST doc Flutter

---
+++ lib/getData/get_data_products.dart
@@ -0,0 +1,41 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+import 'package:flutter/foundation.dart';
+
+List<Product> parseProducts(String responseBody) {
+  final parsed = jsonDecode(responseBody).cast<Map<String, dynamic>>();
+
+  return parsed.map<Product>((json) => Product.fromJson(json)).toList();
+}
+
+Future<List<Product>> fetchProducts(http.Client client) async {
+  final response = await client.get('https://winkels-strapi.herokuapp.com/products');
+
+  return compute(parseProducts, response.body);
+}
+
+class Product {
+  final String productName;
+  String descriptionProduct;
+  final String image;
+
+  Product({this.image, this.productName, this.descriptionProduct});
+
+  factory Product.fromJson(Map<String, dynamic> json) {
+    String imageUrl;
+    Map<String, dynamic> imageJson = json['image'] as Map<String, dynamic>;
+
+    if (imageJson != null) {
+      imageUrl = imageJson['name'] as String;
+    } else {
+      imageUrl = 'https://bubbleerp.sysfosolutions.com/img/default-pro.jpg';
+    }
+
+    return Product(
+      image: imageUrl,
+      productName: json['name'] as String,
+      descriptionProduct: json['description'] as String ?? 'La descripcion de este producto no se encuentra temporalmente',
+    );
+  }
+}
