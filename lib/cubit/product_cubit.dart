f07bc797f65f944ae9d4fa3fddbb9f48742becd0
William Alvis <alvis.wap@gmail.com>
2021-01-27 06:20:21 -0500

    ProductCubit not cry

--- lib/cubit/product_cubit.dart
+++ lib/cubit/product_cubit.dart
@@ -1,22 +1,21 @@
+import 'package:dio/dio.dart';
 import 'package:e_shop_test/cubit/product_state.dart';
+import 'package:e_shop_test/services/networking.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:http/http.dart' as http;
 
-
-class WeatherCubit extends Cubit<ProductsState> {
-  WeatherCubit() : super(InitialState());
-}
 
 class ProductsCubit extends Cubit<ProductsState> {
   ProductsCubit({this.repository}) : super(InitialState()) {
     _getTrendingMovies();
   }
 
-  final MovieRepository repository;
+  final ProductRepository repository;
 
   void _getTrendingMovies() async {
     try {
       emit(LoadingState());
-      final movies = await repository.getMovies();
+      final movies = await repository.fetchProducts(http.Client());
       emit(LoadedState(movies));
     } catch (e) {
       emit(ErrorState());
