12418217c79f51119f64c80e69a0b6f6d6cae740
William Alvis <alvis.wap@gmail.com>
2021-01-26 23:59:35 -0500

    initial implement Cubit patrol

--- lib/cubit/product_cubit.dart
+++ lib/cubit/product_cubit.dart
@@ -1,21 +1,7 @@
-import 'package:cubit/cubit.dart';
-import 'package:movie_app/src/movies/movie_repository.dart';
-import 'package:movie_app/src/movies/movie_state.dart';
+import 'package:e_shop_test/cubit/product_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class MoviesCubit extends Cubit<MoviesState> {
-  MoviesCubit({this.repository}) : super(InitialState()) {
-    _getTrendingMovies();
-  }
 
-  final MovieRepository repository;
-
-  void _getTrendingMovies() async {
-    try {
-      emit(LoadingState());
-      final movies = await repository.getMovies();
-      emit(LoadedState(movies));
-    } catch (e) {
-      emit(ErrorState());
-    }
-  }
+class WeatherCubit extends Cubit<ProductsState> {
+  WeatherCubit() : super(InitialState());
 }
